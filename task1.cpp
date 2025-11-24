7d8c3eb8f31b30c9147b7f4f889cedb51db2c1ea
Toobatariq-cloud <noorulaint9@gmail.com>
2025-11-25 00:31:04 +0500

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n-----------------------------------------------------------";
+	cout<<"\n\tSTUDENT\tID\tCARD\t";
+	cout<<"\n-----------------------------------------------------------";
+	cout<<"\n\tNAME:\t\tTOOBA TARIQ\t";
+	cout<<"\n\tRoll no:\t2025_SET_37\t";
+	cout<<"\n\tDepartment:\tSoftware Enginnering\t";
+	cout<<"\n\tSession:\t2025-2029\t";
+	cout<<"\n-----------------------------------------------------------";
+	return 0;
+}
