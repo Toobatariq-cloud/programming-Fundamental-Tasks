c4cf42339b6b1702523d3f05c1ce9d1f45cf54ac
Toobatariq-cloud <noorulaint9@gmail.com>
2025-11-25 00:52:21 +0500

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\nEnter Student Name:\tTOOBA TARIQ";
+	cout<<"\nEnter Roll No:\t\t2025-SET-37";
+	cout<<"\nEnter Marks in Math:\t93";
+	cout<<"\nEnter Marks in Urdu:\t90";
+	cout<<"\nEnter Marks in Computer:\t70";
+	return 0;
+}
