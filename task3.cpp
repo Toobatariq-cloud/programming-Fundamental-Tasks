836a0d0fdfbafc0184a43f61dcd8dc7de0d3213f
Toobatariq-cloud <noorulaint9@gmail.com>
2025-11-25 00:43:46 +0500

    Add files via upload

---
+++ task3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n------------------------------------------------------------------";
+	cout<<"\n\tCertificate of achievement";
+	cout<<"\nThis is to certify that'Ali Raza'\nHas successfully completed the course\nwith A+ Grade";
+    cout<<"\n------------------------------------------------------------------";
+    return 0;
+}
